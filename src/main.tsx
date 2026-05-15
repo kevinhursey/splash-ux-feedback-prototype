9fb0990a63c7c10eb9675b37f95f844924569d9d
kevinhursey <kforevo@gmail.com>
2026-05-15 10:11:31 -0700

    quick update

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import faviconUrl from './assets/symbol-onestream-2024-1-color-black.svg?url'
+
+const favicon =
+  document.querySelector<HTMLLinkElement>('link[rel="icon"]') ??
+  document.createElement('link')
+favicon.rel = 'icon'
+favicon.type = 'image/svg+xml'
+favicon.href = faviconUrl
+if (!favicon.parentElement) {
+  document.head.appendChild(favicon)
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
